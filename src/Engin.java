3bef48c9e64ec46360c55ad47d62c5e0d2996e8c
dante <shaitanaleksander@gmail.com>
2023-09-28 20:14:20 +0300

    Add fields and constructor Engin

--- src/Engin.java
+++ src/Engin.java
@@ -1,3 +1,12 @@
 public class Engin {
 
+    private  String name;
+    private  int size;
+
+    public Engin(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+
 }
